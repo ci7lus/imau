44646e195d17688bfe14d9b6960d8e75455b6a84
ci7lus <7887955+ci7lus@users.noreply.github.com>
2022-05-22 21:00:07 +0900

    🎨 React18のinitに置き換え

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import { createRoot } from "react-dom/client"
 import { App } from "./App"
 
-ReactDOM.render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 )
