d7e90d51f6c48aadc5c3669a3e82dcc917d5e8cf
SlashNephy <spica@starry.blue>
2022-08-29 22:46:41 +0900

    feat: GraphQL で AniList のユーザーをとってくる

---
+++ src/components/AniListLogin.tsx
@@ -0,0 +1,50 @@
+import { Button, SimpleGrid } from "@mantine/core"
+import { useMemo } from "react"
+import { generateRandomString } from "../utils"
+import { AniListUserInfo } from "./AniListUserInfo"
+
+export const AniListLogin = ({
+  aniListAccessToken,
+  setAniListConnected,
+}: {
+  aniListAccessToken: string
+  setAniListAccessToken: (s: string) => void
+  setAniListConnected: (s: boolean) => void
+}) => {
+  const authUrl = useMemo(() => {
+    const ANILIST_CLIENT_ID = import.meta.env.VITE_ANILIST_CLIENT_ID
+    const ANILIST_REDIRECT_URL = import.meta.env.VITE_ANILIST_REDIRECT_URL
+    if (
+      typeof ANILIST_CLIENT_ID !== "string" ||
+      typeof ANILIST_REDIRECT_URL !== "string"
+    ) {
+      return
+    }
+    const challenge = generateRandomString(50)
+    const state = generateRandomString(10)
+    document.cookie = `challlange=${challenge}`
+    sessionStorage.setItem(state, challenge)
+    const url = new URL("https://anilist.co/api/v2/oauth/authorize")
+    url.searchParams.set("client_id", ANILIST_CLIENT_ID)
+    url.searchParams.set("response_type", "code")
+    url.searchParams.set("state", state)
+    url.searchParams.set("code_challenge", challenge)
+    url.searchParams.set("code_challenge_method", "plain")
+    url.searchParams.set("redirect_uri", ANILIST_REDIRECT_URL)
+    return url.href
+  }, [])
+
+  return (
+    <SimpleGrid>
+      <Button component="a" href={authUrl} color="blue">
+        {aniListAccessToken && "(Re)"}Login with AniList
+      </Button>
+      {aniListAccessToken && (
+        <AniListUserInfo
+          aniListAccessToken={aniListAccessToken}
+          setAniListConnected={setAniListConnected}
+        />
+      )}
+    </SimpleGrid>
+  )
+}
---
+++ src/components/AniListUserInfo.tsx
@@ -0,0 +1,42 @@
+import { Box, Center, Grid, Avatar, Text } from "@mantine/core"
+import { memo, useEffect } from "react"
+import { useQuery } from "react-query"
+import { generateGqlClient } from '../aniListApiEntry'
+
+export const AniListUserInfo = memo(
+  ({
+    aniListAccessToken,
+    setAniListConnected,
+  }: {
+    aniListAccessToken: string
+    setAniListConnected: (s: boolean) => void
+  }) => {
+    const aniList = generateGqlClient(aniListAccessToken)
+    const { data, isLoading } = useQuery(
+      ["ANILIST_PROFILE", aniListAccessToken],
+      () => aniList.getMe(),
+      {
+        cacheTime: 6000,
+        staleTime: 6000,
+      }
+    )
+    useEffect(() => {
+      setAniListConnected(!!data)
+    }, [data])
+    if (isLoading) {
+      return <Center>Loading...</Center>
+    }
+    return (
+      <Box>
+        <Center>
+          <Grid justify="center" align="center" grow>
+            <Avatar src={data?.Viewer?.avatar?.large || ""}></Avatar>
+            <Grid.Col span={2}>
+              <Text>@{data?.Viewer?.name}</Text>
+            </Grid.Col>
+          </Grid>
+        </Center>
+      </Box>
+    )
+  }
+)
